1f9bc6fdb7a2db36eecdcaa58936cee34edb1619
Ray Mitchell <raymonxo@gmail.com>
2013-06-19 22:12:28 -0700

    Renamed project  for Project Euler to Problems.

--- ProjectEuler/Problems/Properties/AssemblyInfo.cs
+++ ProjectEuler/Problems/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ProjectEuler")]
+[assembly: AssemblyTitle("Problems")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ProjectEuler")]
+[assembly: AssemblyProduct("Problems")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
