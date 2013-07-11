f64ec555ce2087453842fa72a7fec03a0ab0119e
Ray Mitchell <raymonxo@gmail.com>
2013-07-10 23:53:44 -0700

    Solved problem 12.

---
+++ ProjectEuler/Problems/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RayMitchell.ProjectEuler
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Problem 11: " + Problem11.Solve());
+            Console.WriteLine("Problem 12: " + Problem12.Solve());
+        }
+    }
+}
