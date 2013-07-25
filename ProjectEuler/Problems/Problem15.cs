2792a0a182d3de32d66c3e093e1242ff92229671
Ray Mitchell <raymonxo@gmail.com>
2013-07-25 01:24:59 -0700

    Solved problem 15.

---
+++ ProjectEuler/Problems/Problem15.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace RayMitchell.ProjectEuler.Problems
+{
+    /// <summary>
+    /// Starting in the top left corner of a 2×2 grid, and only being able
+    /// to move to the right and down, there are exactly 6 routes to the
+    /// bottom right corner.
+    /// 
+    /// How many such routes are there through a 20×20 grid?
+    /// 
+    /// Answer: 137846528820
+    /// </summary>
+    public class Problem15
+    {
+        public static long Solve() { return new Grid(20, 20).Routes; }
+    }
+
+    struct Grid
+    {
+        public long X { get; set; }
+        public long Y { get; set; }
+
+        public Grid(long x, long y) : this() { X = x; Y = y; }
+
+        public long Routes { get { return GetRoutes(this); } }
+
+        private static long GetRoutes(Grid g)
+        {
+            // If cache doesn't contain routes for grid
+            if (!RoutesCache.ContainsKey(g))
+            {
+                // Compute routes for grid
+                long routes = g.X == 0 || g.Y == 0
+                        ? 1
+                        : GetRoutes(new Grid(g.X, g.Y - 1))
+                          + GetRoutes(new Grid(g.X - 1, g.Y));
+
+                // Cache routes for grid and its mirror
+                RoutesCache.Add(g, routes);
+                if (g.X != g.Y)
+                    RoutesCache.Add(new Grid(g.Y, g.X), routes);
+            }
+
+            return RoutesCache[g];
+        }
+
+         private static readonly IDictionary<Grid, long> RoutesCache
+            = new Dictionary<Grid, long>();
+    }
+}
--- ProjectEuler/Problems/Program.cs
+++ ProjectEuler/Problems/Program.cs
@@ -10,6 +10,7 @@ namespace RayMitchell.ProjectEuler.Problems
             Console.WriteLine("Problem 12: " + Problem12.Solve());
             Console.WriteLine("Problem 13: " + Problem13.Solve1());
             Console.WriteLine("Problem 14: " + Problem14.Solve());
+            Console.WriteLine("Problem 15: " + Problem15.Solve());
         }
     }
 }
